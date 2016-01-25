5ba5dc53215002e790e839d93cbbd4d9a0b3c1f9
yguy08 <wyatt.endres@gmail.com>
2016-01-25 14:35:05 -0500

    added more buttons.

--- instigateCuriosity/instigateCuriosity/Information.swift
+++ instigateCuriosity/instigateCuriosity/Information.swift
@@ -15,40 +15,32 @@ import Bolts
 
 class Information: UIViewController, UITextViewDelegate {
     
+    let logOutSuccessMessage = "Logging Out!"
+    let logOutNoUserMessage = "You must be signed in to log out!"
+    
     @IBOutlet weak var informationTextView: UITextView!
     
     
     override func viewWillAppear(animated: Bool) {
         //give text view a delegate
         informationTextView.delegate = self
-
-        
     }
-    
     
     @IBAction func logOutAction(sender: AnyObject) {
-        informationTextView.text = "Logging Out!"
+        informationTextView.text = logOutSuccessMessage
         logOutCurrentParseUser()
+        dispatchToMainScreen()
     }
-    
-    
     
     //logout current user and send them to main
     func logOutCurrentParseUser(){
         if isUserLoggedIn() == true {
             PFUser.logOutInBackground()
             _ = PFUser.currentUser()
-            dispatch_async(dispatch_get_main_queue(), { () -> Void in
-                let viewController:UIViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("Main")
-                self.presentViewController(viewController, animated: true, completion: nil)
-            })
         } else {
-            self.informationTextView.text = "You must be signed in to sign out!"
+            self.informationTextView.text = logOutNoUserMessage
         }
-        
-        
     }
-    
     
     func isUserLoggedIn () ->Bool {
         if (PFUser.currentUser() == nil) {
@@ -57,5 +49,12 @@ class Information: UIViewController, UITextViewDelegate {
             return true
             
         }
+    }
+    
+    func dispatchToMainScreen(){
+        dispatch_async(dispatch_get_main_queue(), { () -> Void in
+            let viewController:UIViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("Main")
+            self.presentViewController(viewController, animated: true, completion: nil)
+        })
     }
 }
